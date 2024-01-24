a2251b1e1d29d694edf3ecf1a6c4042a84f0b20d
Mohamed-Hajri <hajr.kef@gmail.com>
2024-01-24 11:03:13 +0100

    tets

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ git remote set-url origin https://Mohamed-Hajri:github_pat_11BDNI2FA0MogHVZyiINi
 git config --global github.user "Mohamed-Hajri"
 git remote add origin https://Mohamed-Hajri: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@github.com/Mohamed-Hajri/test1.git
 ssh-keygen -t ed25519 -C "hajri.kef@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "hajri.kef@gmail.com"
