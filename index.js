ef6793bcdcf789895cb9bc62f05cd61ee6455925
Mohamed-Hajri <hajr.kef@gmail.com>
2024-01-24 10:51:02 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log(12);
+git remote set-url origin https://Mohamed-Hajri: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@github.com/Mohamed-Hajri/test1.git
+git config --global github.user "Mohamed-Hajri"
+git remote add origin https://Mohamed-Hajri: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@github.com/Mohamed-Hajri/test1.git
+ssh-keygen -t ed25519 -C "hajri.kef@gmail.com"
